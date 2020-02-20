24f054b3e5665906d1d6ec49f68f43e02ae7af2a
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2020-02-20 23:57:43 +0300

    Typings were added

---
+++ src/index.d.ts
@@ -0,0 +1,20 @@
+import {
+  MethodDefinition,
+  InterceptingCall,
+  ServerUnaryCall,
+  ServerWritableStream,
+  ServerReadableStream,
+  ServerDuplexStream
+} from "grpc";
+
+interface InterceptorsFactoryOptions {
+  timeBuckets?: Array<number>;
+}
+
+export function serverInterceptorsFactory(
+  options: InterceptorsFactoryOptions
+): (
+  call: ServerUnaryCall<any> | ServerWritableStream<any> | ServerReadableStream<any> | ServerDuplexStream<any, any>,
+  methodDefinition: MethodDefinition<any, any>,
+  next: Function
+) => Promise<any>;
